e3ad85ff05facb26e7654d9f344413bbfa7eefa0
unknown <UV@FNT-AH07.orizaba.intra.uv.mx>
2023-03-23 15:36:29 -0600

    create base code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           by adsoft <br></br>
+	   Mexico, 2023
         </p>
         <a
           className="App-link"
